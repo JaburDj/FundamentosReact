8faec40b986ff8f447ac562243c54faa391662f2
Daniel Jabur <danieljabur1@gmail.com>
2025-01-15 22:55:59 -0300

    Repositorio inicial

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Post }  from './post';
+
+function App() {
+  return (
+    <div>
+      <Post 
+        author="Daniel jabur" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui vitae illo natus rem veritatis rerum laboriosam ullam debitis laborum, molestias placeat in aliquid commodi? Consectetur iure fugiat non ea quae!"
+      />
+      <Post
+        author="Garcia"
+        content="um novo progamador"
+      
+      />
+
+      <Post
+
+        author="Jabur"  
+        content="O cara"
+        />
+
+        <Post></Post> 
+      
+    </div>
+  )
+}
+
+export default App
+
+
